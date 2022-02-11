22974cbe852fb89041dc5b4bcbf13d55739e509a
Riyad Mney <rimney@e2r9p2.1337.ma>
2022-02-11 04:56:12 +0100

    norminette done

--- rotate.c
+++ rotate.c
@@ -6,44 +6,43 @@
 /*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 10:47:29 by rimney            #+#    #+#             */
-/*   Updated: 2022/02/11 04:29:13 by rimney           ###   ########.fr       */
+/*   Updated: 2022/02/11 04:44:36 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-static void ft_rotate(int *tab, int size)
+static void	ft_rotate(int *tab, int size)
 {
-    int first = tab[0];
-    int swap;
-    int i;
+	int	first;
+	int	swap;
+	int	i;
 
-    i = 1;
-    while(i < size)
-    {
-        swap = tab[i];
-        tab[i] = tab[i - 1];
-        tab[i - 1] = swap;
-        i++;
-    }
-    tab[size] = 0;
+	i = 1;
+	first = tab[0];
+	while (i < size)
+	{
+		swap = tab[i];
+		tab[i] = tab[i - 1];
+		tab[i - 1] = swap;
+		i++;
+	}
+	tab[size] = 0;
 }
 
-void    ft_ra(t_swap *s)
+void	ft_ra(t_swap *s)
 {
-    ft_rotate(s->stack_a, s->stack_a_size);
-    ft_print("ra\n");
+	ft_rotate(s->stack_a, s->stack_a_size);
+	ft_print("ra\n");
 }
 
-void    ft_rb(t_swap *s)
+void	ft_rb(t_swap *s)
 {
-    ft_rotate(s->stack_b, s->stack_b_size);
-    ft_print("rb\n");
+	ft_rotate(s->stack_b, s->stack_b_size);
+	ft_print("rb\n");
 }
 
-void    ft_rr(t_swap *s)
+void	ft_rr(t_swap *s)
 {
-    ft_rotate(s->stack_a, s->stack_a_size);
-    ft_rotate(s->stack_b, s->stack_b_size);
-    ft_print("rr\n");
+	ft_rotate(s->stack_a, s->stack_a_size);
+	ft_rotate(s->stack_b, s->stack_b_size);
+	ft_print("rr\n");
 }
--- swap.c
+++ swap.c
@@ -6,37 +6,36 @@
 /*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 10:47:34 by rimney            #+#    #+#             */
-/*   Updated: 2022/02/11 04:09:41 by rimney           ###   ########.fr       */
+/*   Updated: 2022/02/11 04:55:37 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void ft_swap(int *a, int *b)
+static void	ft_swap(int *a, int *b)
 {
-    int c;
+	int	c;
 
-    c = *a;
-    *a = *b;
-    *b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
 
-void    ft_sa(t_swap *s)
+void	ft_sa(t_swap *s)
 {
-    ft_swap(&s->stack_a[0], &s->stack_a[1]);
-    ft_print("sa\n");
+	ft_swap(&s->stack_a[0], &s->stack_a[1]);
+	ft_print("sa\n");
 }
 
-void    ft_sb(t_swap *s)
+void	ft_sb(t_swap *s)
 {
-    ft_swap(&s->stack_b[0], &s->stack_b[1]);
-    ft_print("sb\n");
+	ft_swap(&s->stack_b[0], &s->stack_b[1]);
+	ft_print("sb\n");
 }
 
-void    ft_ss(t_swap *s)
+void	ft_ss(t_swap *s)
 {
-    ft_sa(s);
-    ft_sb(s);
-    ft_print("ss\n");
+	ft_sa(s);
+	ft_sb(s);
+	ft_print("ss\n");
 }
-
